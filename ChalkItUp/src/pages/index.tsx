42503c93e13563063a3ebcf93e44e8354bba319d
jonathan.fidan@adesso.de <jonathan.fidan@adesso.de>
2025-01-29 14:15:35 +0100

    .

--- ChalkItUp/src/pages/index.tsx
+++ ChalkItUp/src/pages/index.tsx
@@ -3,7 +3,6 @@ import { Snippet } from "@heroui/snippet";
 import { Code } from "@heroui/code";
 import { button as buttonStyles } from "@heroui/theme";
 
-import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
