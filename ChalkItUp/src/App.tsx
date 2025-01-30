cdb130e1bd825d79f66bf2e4f1ab28cf371abcbb
gabriel.akbarov@adesso.de <gabriel.akbarov@adesso.de>
2025-01-30 13:50:55 +0100

    unfinished

--- ChalkItUp/src/App.tsx
+++ ChalkItUp/src/App.tsx
@@ -1,14 +1,11 @@
-import { Route, Routes } from 'react-router-dom';
+import Login from "@/components/auth/login.tsx";
 
-import IndexPage from '@/pages/index';
-import PlayerPage from './pages/player';
 
 function App() {
     return (
-        <Routes>
-            <Route element={<IndexPage />} path="/" />
-            <Route element={<PlayerPage />} path="/players" />
-        </Routes>
+       <>
+       <Login/>
+       </>
     );
 }
 
