1abadcfcc25f2e35c5467472ed63503d88233fc0
Gabriel Akbarov <kontakt@gabrielakbarov.de>
2025-01-30 14:24:22 +0100

    Update firebase.ts

--- ChalkItUp/src/firebase/firebase.ts
+++ ChalkItUp/src/firebase/firebase.ts
@@ -2,13 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAI8BYekdwhBjlXUAz8Myti3Z73x1mtyQQ",
-    authDomain: "chalkitup-95340.firebaseapp.com",
-    projectId: "chalkitup-95340",
-    storageBucket: "chalkitup-95340.firebasestorage.app",
-    messagingSenderId: "536463964659",
-    appId: "1:536463964659:web:9d327419028932881bba86",
-    measurementId: "G-GEV22723D1"
+
 };
 
 const app = initializeApp(firebaseConfig);
