f89f337b83a3d37e4d1eafb33984973752921472
jonathan.fidan@adesso.de <jonathan.fidan@adesso.de>
2025-01-30 09:35:20 +0100

    typo

--- ChalkItUp/src/App.tsx
+++ ChalkItUp/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     return (
         <Routes>
             <Route element={<IndexPage />} path="/" />
-            <Route element={<PlayerPage />} path="/player" />
+            <Route element={<PlayerPage />} path="/players" />
         </Routes>
     );
 }
