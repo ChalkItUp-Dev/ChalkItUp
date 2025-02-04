8ba597fe25cce6a1949d1264c7d5165b14f9199c
jonathan.fidan@adesso.de <jonathan.fidan@adesso.de>
2025-02-04 14:41:13 +0100

    fix

--- ChalkItUp/src/App.tsx
+++ ChalkItUp/src/App.tsx
@@ -1,5 +1,5 @@
 import LoginPage from './pages/login.tsx';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import RegisterPage from './pages/register.tsx';
 import { AuthProvider, useAuth } from './contexts/authContext/index.tsx';
@@ -9,45 +9,43 @@ import PlayerPage from '@/pages/player.tsx';
 const App = () => {
     return (
         <AuthProvider>
-            <BrowserRouter>
-                <Routes>
-                    {/* Login & Register nur für nicht eingeloggte Nutzer */}
-                    <Route
-                        path="/login"
-                        element={
-                            <RedirectIfAuthenticated>
-                                <LoginPage />
-                            </RedirectIfAuthenticated>
-                        }
-                    />
-                    <Route
-                        path="/register"
-                        element={
-                            <RedirectIfAuthenticated>
-                                <RegisterPage />
-                            </RedirectIfAuthenticated>
-                        }
-                    />
+            <Routes>
+                {/* Login & Register nur für nicht eingeloggte Nutzer */}
+                <Route
+                    path="/login"
+                    element={
+                        <RedirectIfAuthenticated>
+                            <LoginPage />
+                        </RedirectIfAuthenticated>
+                    }
+                />
+                <Route
+                    path="/register"
+                    element={
+                        <RedirectIfAuthenticated>
+                            <RegisterPage />
+                        </RedirectIfAuthenticated>
+                    }
+                />
 
-                    {/* Geschützte Routen */}
-                    <Route
-                        path="/"
-                        element={
-                            <ProtectedRoute>
-                                <IndexPage />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="/players"
-                        element={
-                            <ProtectedRoute>
-                                <PlayerPage />
-                            </ProtectedRoute>
-                        }
-                    />
-                </Routes>
-            </BrowserRouter>
+                {/* Geschützte Routen */}
+                <Route
+                    path="/"
+                    element={
+                        <ProtectedRoute>
+                            <IndexPage />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/players"
+                    element={
+                        <ProtectedRoute>
+                            <PlayerPage />
+                        </ProtectedRoute>
+                    }
+                />
+            </Routes>
         </AuthProvider>
     );
 };
--- ChalkItUp/src/main.tsx
+++ ChalkItUp/src/main.tsx
@@ -1,5 +1,16 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "@/styles/globals.css";
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import '@/styles/globals.css';
+import { Provider } from './provider.tsx';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App/>);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider>
+                <App />
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>
+);
