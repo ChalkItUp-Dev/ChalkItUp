b3e68badf3b9de84bba99cfcd0792a768909b962
gabriel.akbarov@adesso.de <gabriel.akbarov@adesso.de>
2025-02-04 13:57:14 +0100

    fixed warnings

--- ChalkItUp/src/components/auth/logOut.tsx
+++ ChalkItUp/src/components/auth/logOut.tsx
@@ -1,17 +1,12 @@
 import {Button} from "@heroui/button";
-import { useAuth } from "@/contexts/authContext";
-import { doSignOut } from "@/firebase/auth.ts";
+import {useAuth} from "@/contexts/authContext";
+import {doSignOut} from "@/firebase/auth.ts";
 
 const LogoutButton = () => {
-    const { currentUser } = useAuth();
+    const {currentUser} = useAuth();
 
     const handleLogout = async () => {
-        try {
-            await doSignOut();
-        } catch (error) {
-            // @ts-ignore
-            console.error("Logout failed:", error.message);
-        }
+        await doSignOut();
     };
 
     if (!currentUser) return null;
